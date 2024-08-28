330d00af5d35b03baf5e2505b547b40e3730a7ba
Adhishthan <adhishthanashok@gmail.com>
2024-08-28 11:13:40 +0530

    Added toastcontainer

--- frontend/src/components/SignInForm.jsx
+++ frontend/src/components/SignInForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 
 const SignInForm = () => {
   const [formData, setFormData] = useState({
